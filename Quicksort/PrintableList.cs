5c30c2c2088fee0440a9f03ea8b5a91da696120c
NemoNessuno <gokutwok@gmx.de>
2011-05-01 20:55:16 +0200

    Example Upload

---
+++ Quicksort/PrintableList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quicksort
+{
+    class PrintableList<T>
+    {
+        public T[] ts;
+
+        public PrintableList(int size)
+        {
+            this.ts = new T[size];
+        }
+
+        //Indexer Definition
+        public T this[int i]
+        {
+            get
+            {
+                return ts[i];
+            }
+            set
+            {
+                ts[i] = value;
+            }
+        }
+
+        public void Print()
+        {
+            foreach (T t in ts) Console.WriteLine(t);
+        }
+    }
+}
